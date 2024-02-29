e7b3d4977f2e8594d4c8a010f27ec1489993dfb1
Eduardo Esteban Alvarez Castañeda <soy@eduardoalvarez.dev>
2024-02-29 04:23:12 -0300

    revert: change header

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -7,7 +7,17 @@ const PrincipalLayout = ({ children }: PropsWithChildren) => {
     <div className="w-screen h-screen grid grid-rows-[auto,1fr,auto] overflow-x-hidden">
       <Header />
 
-      <main className="w-full lg:w-4/5 max-w-4xl mx-auto px-10 flex flex-col gap-2 my-5 xl:mt-40">
+      <main className="w-full max-w-4xl flex flex-col gap-2 mx-auto px-10 mt-8 mb-24 lg:w-4/5 md:mb-0 md:mt-20">
+        <hgroup className="flex flex-col my-5">
+          <h1 className="text-4xl font-bold text-center">
+            Color Contrast Checker
+          </h1>
+
+          <h2 className="text-center text-gray-500">
+            Check the contrast ratio between text and background colors
+          </h2>
+        </hgroup>
+
         <div className="flex flex-col gap-2">{children}</div>
       </main>
 
