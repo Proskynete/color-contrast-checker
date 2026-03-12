3fdf6cd0e1d7056fbb7b21ec1913c4ec367d3825
Eduardo Esteban Alvarez Castañeda <soy@eduardoalvarez.dev>
2026-03-12 19:08:30 -0300

    build: ➕ add @astrojs/node and switch to SSR output mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
+import node from '@astrojs/node';
 import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+	output: 'server',
+	adapter: node({ mode: 'standalone' }),
 	integrations: [tailwind(), react()],
 });
